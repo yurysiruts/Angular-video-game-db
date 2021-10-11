62cce5933932baf8440aaa69c89f737750629dcf
yurysiruts <sirut.2003@gmail.com>
2021-10-12 01:13:35 +0200

    implementing fomr compponent

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { APIResponse, Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +11,30 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   public sort: string;
+  public games: Array<Game>;
   
-  constructor() {}
+  constructor(
+    private httpSerice: HttpService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      if(params['game-search']) {
+        this.searchGames('metacrit', params['game-search']);
+      } else {
+        this.searchGames('metacrit');
+      }
+    });
+  }
+
+  searchGames(sort: string, search?: string): void {
+    this.httpSerice
+      .getGameList(sort, search)
+      .subscribe((gameList: APIResponse<Game>) => {
+        this.games = gameList.results;
+        console.log(gameList);
+      });
   }
 
 }
--- src/app/intercepters/http-headers.interceptor.ts
+++ src/app/intercepters/http-headers.interceptor.ts
@@ -8,12 +8,14 @@ export class HttpHeadersInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     req = req.clone({
+      // headers from rapidAPI
       setHeaders: {
-          'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
-          'x-rapidapi-key': 'f776343bd9msh8fddee62241142bp1d4e76jsn1704b583a3a2'
+        'x-rapidapi-key': 'f776343bd9msh8fddee62241142bp1d4e76jsn1704b583a3a2',
+        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
       },
+      // params from RAWG db API
       setParams: {
-        key: 'f776343bd9msh8fddee62241142bp1d4e76jsn1704b583a3a2',
+         key: '153b8274a3e94866a57f22f127207b80',
       }
     });
     return next.handle(req);
